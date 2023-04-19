ba7dc22c818514f54a874a48c996b60dd68c3ca9
Hans <classsickpeople@gmail.com>
2023-04-20 01:48:48 +0800

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import ShowCard from './components/ShowCard.vue'
+</script>
+
+<template>
+    <div>
+        <ShowCard v-for="(item, index) in shows" v-bind="item" :key="index"/>
+        <h1 class="text-red-700">a</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            shows: []
+        }
+    },
+    mounted () {
+        this.getShows()
+    },
+    methods: {
+        async getShows () {
+            const response = await fetch('http://api.tvmaze.com/search/shows?q=girls')
+            const responseJson = await response.json()
+            this.shows = responseJson
+        }
+    }
+}
+</script>
---
+++ src/components/ShowCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <img
+            :src="show.image.original"
+            :alt="show.name"
+            :title="show.name"
+            class="w-full"
+        >
+        <h1 class="text-red-700">{{ show.name }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        score: {
+            type: Number
+        },
+        show: {
+            type: Object
+        }
+    }
+}
+</script>
